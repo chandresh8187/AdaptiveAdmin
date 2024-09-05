f14540e2b044b8ba92159589c162c37bff4e4087
chandresh8187 <borichachandresh35@gmail.com>
2024-09-05 17:17:15 +0530

    removed unused

--- src/component/AIContainer/Box.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./Box.css";
-function Box() {
-  return <div className="BoxContainer">Box</div>;
-}
-
-export default Box;
--- src/component/AIContainer/Container.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./Container.css";
-function Container({ children }) {
-  return <div className="MainContainer">{children}</div>;
-}
-
-export default Container;
